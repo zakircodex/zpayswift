c1e03e402f06d450471fe83e47202fd8e666251b
zakircodex <zakirblogsmalaysia@gmail.com>
2026-06-17 00:03:18 +0800

    Add Z Builder BD OTP owner auth APIs

---
+++ api/my_site/owner_logout.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/../bootstrap.php';
+require_once __DIR__ . '/_owner_auth.php';
+
+api_require_method('POST');
+
+$token = zb_session_token_from_request();
+if ($token !== '') {
+    fb_patch('Z_BUILDER_OWNER_SESSIONS/' . zb_token_hash($token), [
+        'status' => 'LOGGED_OUT',
+        'logged_out_at' => zb_now_iso(),
+        'updated_at' => zb_now_iso(),
+    ]);
+}
+
+api_response(true, 'SUCCESS', 'Logged out', []);
