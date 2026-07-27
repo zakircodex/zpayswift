025edd646d57ac894b8a53b1daf6979dd5524cfb
zakircodex <zakirblogsmalaysia@gmail.com>
2026-07-27 21:53:02 +0800

    feat(znews): add public post details endpoint

---
+++ api/znews/public/post.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+require_once dirname(__DIR__) . '/bootstrap.php';
+require_once dirname(__DIR__) . '/lib/posts.php';
+require_once dirname(__DIR__) . '/lib/post_access.php';
+
+api_require_method('GET');
+
+$postId = znews_firebase_key($_GET['post_id'] ?? '', 'post_id');
+$post = znews_public_post_by_id($postId);
+
+if (!is_array($post)) {
+    api_response(false, 'ZNEWS_POST_NOT_FOUND', 'Post not found.', [], 404);
+}
+
+api_response(true, 'ZNEWS_PUBLIC_POST_OK', 'Post loaded.', ['post' => $post]);
