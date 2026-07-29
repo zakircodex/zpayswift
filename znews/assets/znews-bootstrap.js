b1d3d53e37a30b10092bdbdd7eceb36137046218
zakircodex <zakirblogsmalaysia@gmail.com>
2026-07-30 01:25:14 +0800

    feat(znews): load post reader module before app boot

--- znews/assets/znews-bootstrap.js
+++ znews/assets/znews-bootstrap.js
@@ -75,10 +75,11 @@
     await loadScript('/znews/assets/znews-access.js?v=1');
     await loadScript('/znews/assets/znews-feed-ui.js?v=1');
     await loadScript('/znews/assets/znews-profile.js?v=1');
+    await loadScript('/znews/assets/znews-reader.js?v=1');
     await loadScript('/znews/assets/znews.js?v=3');
     await loadScript('/znews/assets/znews-header.js?v=2');
     await loadScript('/znews/assets/znews-creator.js?v=2');
-    await loadScript('/znews/assets/znews-instant-comments.js?v=2');
+    await loadScript('/znews/assets/znews-instant-comments.js?v=3');
     document.documentElement.classList.add('znews-ready');
   }
 
